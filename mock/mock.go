f2c68eba15bd58ff52a693877d61b926276e090d
SoundMatt <47545907+SoundMatt@users.noreply.github.com>
2026-06-16 16:38:25 -0700

    feat(mock): add mock sub-package alias for virtual bus (closes #18)
    RELAY spec §7 requires every implementation to ship a sub-package named
    'mock'. Adds mock.Bus (type alias of virtual.Bus) and mock.New() so
    callers can import github.com/SoundMatt/go-CAN/mock without any
    behavioural difference from virtual.
    
    Signed-off-by: SoundMatt <47545907+SoundMatt@users.noreply.github.com>

---
+++ mock/mock.go
@@ -0,0 +1,18 @@
+// Copyright (c) 2026 Matt Jones. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+// Package mock provides an in-process mock CAN bus for testing.
+// It satisfies the RELAY spec §7 requirement that every implementation
+// ship a sub-package named "mock". The mock is identical to the virtual
+// package and implements all mandatory interfaces.
+package mock
+
+import "github.com/SoundMatt/go-CAN/virtual"
+
+// Bus is the mock CAN bus. It is identical to virtual.Bus.
+type Bus = virtual.Bus
+
+// New creates an in-process mock CAN bus.
+func New() (*Bus, error) { return virtual.New() }
---
+++ mock/mock_test.go
@@ -0,0 +1,39 @@
+// Copyright (c) 2026 Matt Jones. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+package mock_test
+
+import (
+	"context"
+	"testing"
+
+	can "github.com/SoundMatt/go-CAN"
+	"github.com/SoundMatt/go-CAN/mock"
+)
+
+func TestMockImplementsBus(t *testing.T) {
+	b, err := mock.New()
+	if err != nil {
+		t.Fatalf("New: %v", err)
+	}
+	defer b.Close()
+
+	// Verify mock.Bus satisfies can.Bus interface.
+	var _ can.Bus = b
+
+	ch, err := b.Subscribe()
+	if err != nil {
+		t.Fatalf("Subscribe: %v", err)
+	}
+
+	if err := b.Send(context.Background(), can.Frame{ID: 0x100, Data: []byte{0x01}}); err != nil {
+		t.Fatalf("Send: %v", err)
+	}
+
+	f := <-ch
+	if f.ID != 0x100 {
+		t.Errorf("got ID 0x%X, want 0x100", f.ID)
+	}
+}
